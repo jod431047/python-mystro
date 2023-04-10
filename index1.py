4ad41e85c170f579dfb4fd7c028f11f92c21c49b
jod431047 <jod431047@gmail.com>
2023-04-10 03:53:31 +0200

    python

--- index1.py
+++ index1.py
@@ -9,28 +9,33 @@ games :
 -sentence no duplicate
 -names ---> list[count]
 -x,y :1-->100
-''' 
+'''
 
 
 from typing import Self
 
 
-class Game :
+class Game:
     def __init__(self):
         print('''welcome to our game
         1 :Sentence No Duplicate
         2 :Names List Count
         3 :Divided By
-        ''') 
-    user_choice =int (input('Enter Game Number :'))
-    if user_choice == 1 :
+        ''')
+    user_choice = int(input('Enter Game Number :'))
+    if user_choice == 1:
         sentence = input('Enter sentence :')
     Self.no_duplicate()
+    
+    elif user_choice == 2:
+    names = list(input('Enter Names:'))
+    print(names)
+    Self.names_count(names)
 
-    def no_duplicate(self,sentence):
+    def no_duplicate(self, sentence):
         words = sentence.split(' ')
-        new_words =[]
-        for w in words :
+        new_words = []
+        for w in words:
             if w in new_words:
                 continue
             else:
@@ -39,27 +44,23 @@ class Game :
                 new_sentence = ' '.join(new_words)
                 print(new_sentence)
 
-    def names_count(self,names):
+    def names_count(self, names):
         count = []
         for n in names:
             count.append(len(n))
-        print(count) 
+        print(count)
 
-
-    def divided_by(self,x,y):
-        result =[]
-        for n in range(1,101):
-            if n%x ==0 and n%y == 0:
+    def divided_by(self, x, y):
+        result = []
+        for n in range(1, 101):
+            if n % x == 0 and n % y == 0:
                 result.append(n)
-        print(result)   
+        print(result)
+
 
 g1 = Game()
 
 
-
-
-
-
-#duplicate('my name is is is jihad') 
-#names_count(['ahmad','ali','jihad'])   
-#divided_by(5,6)
+# duplicate('my name is is is jihad')
+# names_count(['ahmad','ali','jihad'])
+# divided_by(5,6)
